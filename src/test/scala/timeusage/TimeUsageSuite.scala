4cad70edb348f980eba9f357c53db81d3f456d64
younss <younssiga@gmail.com>
2018-08-24 01:09:46 -0400

    improve Unit Test

--- src/test/scala/timeusage/TimeUsageSuite.scala
+++ src/test/scala/timeusage/TimeUsageSuite.scala
@@ -1,12 +1,8 @@
 package timeusage
 
+import org.apache.avro.generic.GenericData
 import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import org.apache.spark.sql.types.{
-  DoubleType,
-  StringType,
-  StructField,
-  StructType
-}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
@@ -17,11 +13,11 @@ import scala.util.Random
 class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
   import TimeUsage._
 
-    test("dfSchema method returns a StructType describing the schema of the CSV file") {
-    val firstLine = List("A","B","C","D")
+    test("dfSchema method returns a StructType describing the schema of the CSV file first Field is String, others Are Double") {
+    val firstLine = List("A","B")
       val result = dfSchema(firstLine)
-      assert(result.head.dataType.eq(StringType))
-      assert(result.tail.head.dataType.eq(DoubleType))
+      assert(result.contains(StructField("A",StringType,false)))
+      assert(result.contains(StructField("B",DoubleType,false)))
 
   }
 }
